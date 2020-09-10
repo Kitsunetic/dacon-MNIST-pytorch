a811642da36f1de27c231566869291ceba3de158
Kitsunetic <kitsune.guru@gmail.com>
2020-09-10 22:36:15 +0900

    resnext

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from efficientnet_pytorch import EfficientNet
 def make_model(model_name_: str) -> nn.Module:
     model_name = model_name_.lower()
 
-    if model_name.startswith('resnet'):
+    if model_name.startswith('resnet') or model_name.startswith('resnext'):
         model: nn.Module = getattr(modified_resnet, model_name)(pretrained=False, progress=False, num_classes=10)
     elif model_name.startswith('efficientnet'):
         model: nn.Module = EfficientNet.from_name(model_name, in_channels=2)
--- modified_resnet.py
+++ modified_resnet.py
@@ -45,7 +45,7 @@ class BasicBlock(nn.Module):
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
         # self.relu = nn.ReLU(inplace=True)
-        self.relu = nn.ELU(inplace=True)
+        self.relu = nn.LeakyReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
@@ -101,7 +101,7 @@ class Bottleneck(nn.Module):
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
         # self.relu = nn.ReLU(inplace=True)
-        self.relu = nn.ELU(inplace=True)
+        self.relu = nn.LeakyReLU(inplace=True)
 
         self.downsample = downsample
         self.stride = stride
@@ -154,7 +154,7 @@ class ResNet(nn.Module):
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         # self.relu = nn.ReLU(inplace=True)
-        self.relu = nn.ELU(inplace=True)
+        self.relu = nn.LeakyReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
